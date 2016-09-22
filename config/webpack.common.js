e75b0b8aaacadcf736ad0d698532db5c763b13d2
Christian Schrödel <schroedel.christian@googlemail.com>
2016-09-22 17:04:02 +0200

    Switched buildchain to webpack
    Refactored project src code

---
+++ config/webpack.common.js
@@ -0,0 +1,50 @@
+/**
+ * Created by developer on 19.09.2016.
+ */
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var helpers = require('./helpers');
+
+module.exports = {
+  entry: {
+    'polyfills': './src/polyfills.ts',
+    'vendor': './src/vendor.ts',
+    'app': './src/app/main.ts'
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.ts']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file?name=assets/[name].[hash].[ext]'
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: 'raw-loader!sass-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor', 'polyfills']
+    }),
+
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {MainComponent} from './app.component';
+import {HelloWorldComponent} from './components/hello-world/hello-world.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [MainComponent],
-  bootstrap: [MainComponent]
+  declarations: [MainComponent, HelloWorldComponent],
+  bootstrap: [MainComponent],
 })
 export class MainModule {
 }
